becb753e3fd1129d4246793edc6f1b23e9c8f10d
Doug Wollison <doug@premise.ca>
2019-12-19 16:47:49 -0500

    convert Board to functional component; no state needed

--- assets/js/components/board.js
+++ assets/js/components/board.js
@@ -3,41 +3,33 @@ import React from 'react';
 import Square from './square.js';
 import Token from './token.js';
 
-export default class Board extends React.Component {
-	constructor( props ) {
-		super( props );
+export default function Board( { width, height, squares, tokens, onPlay } ) {
+	function getProps( config ) {
+		return {
+			side: config.side,
+			top: ( config.top / height ) * 100 + '%',
+			left: ( config.left / width ) * 100 + '%',
+		};
 	}
 
-	render() {
-		const { width, height, squares, tokens, onPlay } = this.props;
+	const theSquares = squares.map( config => {
+		return ( <Square { ...getProps( config ) } /> );
+	} );
 
-		function getProps( config ) {
-			return {
-				side: config.side,
-				top: ( config.top / height ) * 100 + '%',
-				left: ( config.left / width ) * 100 + '%',
-			};
+	const theTokens = tokens.map( config => {
+		// skip tokens that don't have a position
+		if ( typeof config.top === 'undefined' ) {
+			return null;
 		}
 
-		const theSquares = squares.map( config => {
-			return ( <Square { ...getProps( config ) } /> );
-		} );
+		return ( <Token { ...getProps( config ) } onClick={ () => onPlay( config ) } /> );
+	} );
 
-		const theTokens = tokens.map( config => {
-			// skip tokens that don't have a position
-			if ( typeof config.top === 'undefined' ) {
-				return null;
-			}
-
-			return ( <Token { ...getProps( config ) } onClick={ () => onPlay( config ) } /> );
-		} );
-
-		return (
-			<div className="ur-board">
-				<svg version="1" width={ width * 100 } height={ height * 100 } xmlns="http://www.w3.org/2000/svg"></svg>
-				{ theSquares }
-				{ theTokens }
-			</div>
-		);
-	}
+	return (
+		<div className="ur-board">
+			<svg version="1" width={ width * 100 } height={ height * 100 } xmlns="http://www.w3.org/2000/svg"></svg>
+			{ theSquares }
+			{ theTokens }
+		</div>
+	);
 }
