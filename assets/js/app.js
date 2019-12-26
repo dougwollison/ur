a6709ffce7e2e81b777c8d23cc052f2998753117
Doug Wollison <doug@wollison.net>
2019-12-26 11:01:25 -0500

    rewrite squares config for collide mode
    red/blue go in opposite directions rather than racing, arrows show for current player's flow, lengthen board so safe square is dead centre
    
    TODO: figure out handling of flipped direction while also preserving capturing

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,34 +16,35 @@ const GAME_SQUARES = [
 	========== */
 
 	// Left Squares
-	{ index: -1, side: 0, left: 0, top: 4, arrow: '', isStart: true },
-	{ index: 0, side: 0, left: 0, top: 3, arrow: 'u v' },
-	{ index: 1, side: 0, left: 0, top: 2, arrow: 'u v' },
-	{ index: 2, side: 0, left: 0, top: 1, arrow: 'u v' },
-	{ index: 3, side: 0, left: 0, top: 0, arrow: 'r br', isDouble: true },
-	{ index: 12, side: 0, left: 0, top: 7, arrow: 'u tr' },
-	{ index: 13, side: 0, left: 0, top: 6, arrow: 'u v', isDouble: true },
-	{ index: 14, side: 0, left: 0, top: 5, arrow: '', isEnd: true },
+	{ index: -1, side: 0, left: 0, top: 4, arrowSide1: '', isStart: true },
+	{ index: 0, side: 0, left: 0, top: 3, arrowSide1: 'u v' },
+	{ index: 1, side: 0, left: 0, top: 2, arrowSide1: 'u v' },
+	{ index: 2, side: 0, left: 0, top: 1, arrowSide1: 'u v' },
+	{ index: 3, side: 0, left: 0, top: 0, arrowSide1: 'r br', isDouble: true },
+	{ index: 13, side: 0, left: 0, top: 8, arrowSide1: 'u tr' },
+	{ index: 14, side: 0, left: 0, top: 7, arrowSide1: 'u v', isDouble: true },
+	{ index: 15, side: 0, left: 0, top: 6, arrowSide1: '', isEnd: true },
 
 	// Right Squares
-	{ index: -1, side: 1, left: 2, top: 4, arrow: '', isStart: true },
-	{ index: 0, side: 1, left: 2, top: 3, arrow: 'u v' },
-	{ index: 1, side: 1, left: 2, top: 2, arrow: 'u v' },
-	{ index: 2, side: 1, left: 2, top: 1, arrow: 'u v' },
-	{ index: 3, side: 1, left: 2, top: 0, arrow: 'l bl', isDouble: true },
-	{ index: 12, side: 1, left: 2, top: 7, arrow: 'u tl' },
-	{ index: 13, side: 1, left: 2, top: 6, arrow: 'u v', isDouble: true },
-	{ index: 14, side: 1, left: 2, top: 5, arrow: '', isEnd: true },
+	{ index: -1, side: 1, left: 2, top: 4, arrowSide2: '', isStart: true },
+	{ index: 0, side: 1, left: 2, top: 5, arrowSide2: 'd v' },
+	{ index: 1, side: 1, left: 2, top: 6, arrowSide2: 'd v' },
+	{ index: 2, side: 1, left: 2, top: 7, arrowSide2: 'd v' },
+	{ index: 3, side: 1, left: 2, top: 8, arrowSide2: 'l tl', isDouble: true },
+	{ index: 13, side: 1, left: 2, top: 0, arrowSide2: 'd bl' },
+	{ index: 14, side: 1, left: 2, top: 1, arrowSide2: 'd v', isDouble: true },
+	{ index: 15, side: 1, left: 2, top: 2, arrowSide2: '', isEnd: true },
 
 	// Middle/Combat Squares
-	{ index: 4, side: -1, left: 1, top: 0, arrow: 'd bl br' },
-	{ index: 5, side: -1, left: 1, top: 1, arrow: 'd v' },
-	{ index: 6, side: -1, left: 1, top: 2, arrow: 'd v' },
-	{ index: 7, side: -1, left: 1, top: 3, arrow: 'd v' },
-	{ index: 8, side: -1, left: 1, top: 4, arrow: 'd v', isDouble: true, isSafe: true },
-	{ index: 9, side: -1, left: 1, top: 5, arrow: 'd v' },
-	{ index: 10, side: -1, left: 1, top: 6, arrow: 'd v' },
-	{ index: 11, side: -1, left: 1, top: 7, arrow: 'l r tl tr' },
+	{ index: 4, side: -1, left: 1, top: 0, arrowSide1: 'd bl', arrowSide2: 'r br' },
+	{ index: 5, side: -1, left: 1, top: 1, arrowSide1: 'd v', arrowSide2: 'u v' },
+	{ index: 6, side: -1, left: 1, top: 2, arrowSide1: 'd v', arrowSide2: 'u v' },
+	{ index: 7, side: -1, left: 1, top: 3, arrowSide1: 'd v', arrowSide2: 'u v' },
+	{ index: 8, side: -1, left: 1, top: 4, arrowSide1: 'd v', arrowSide2: 'u v', isDouble: true, isSafe: true },
+	{ index: 9, side: -1, left: 1, top: 5, arrowSide1: 'd v', arrowSide2: 'u v' },
+	{ index: 10, side: -1, left: 1, top: 6, arrowSide1: 'd v', arrowSide2: 'u v' },
+	{ index: 11, side: -1, left: 1, top: 7, arrowSide1: 'd v', arrowSide2: 'u v' },
+	{ index: 12, side: -1, left: 1, top: 8, arrowSide1: 'l tl', arrowSide2: 'u tr' },
 ];
 
 const gameConfig = {
@@ -54,7 +55,7 @@ const gameConfig = {
 	tokenCount: 7,
 	boardConfig: {
 		cols: 3,
-		rows: 8,
+		rows: 9,
 	},
 };
 
